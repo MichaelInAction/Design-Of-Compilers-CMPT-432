a04e5310aeb23d7a1b27f1fec5f9fe33cbbde2c0
MichaelInAction <michael.read.75@gmail.com>
2018-03-04 10:38:45 -0500

    3/4/2018 - Project2
    Added tree creation to parse.js, and completed my recursive reading function in index.html.

--- Project2/parse.js
+++ Project2/parse.js
@@ -1,12 +1,12 @@
 var tokens;
-var tree;
+var CurrentNode;
 var warnings;
 var errors;
 
-function parseProgram(tokens, tree, warnings, errors){
+function parseProgram(tokens, FirstNode, warnings, errors){
   console.log("Begin Parsing...");
   this.tokens = tokens;
-  this.tree = tree;
+  CurrentNode = FirstNode;
   this.warnings = warnings;
   this.errors = errors;
   parseBlock();
@@ -17,7 +17,8 @@ function matchToken(token, expectedValue){
   if(errors.length == 0){
     if(token.type == expectedValue){
       console.log("Match found");
-      tree.push(token.type);
+      var NewNode = {parent:CurrentNode, value:token.value, children:[]};
+      CurrentNode.children.push(NewNode);
     }
     else{
       console.log("Error: Found " + token.type + "; expected: " + expectedValue);
@@ -28,12 +29,19 @@ function matchToken(token, expectedValue){
 }
 
 function parseBlock(){
-  matchToken(tokens.shift(), "OPEN_BRACKET_TOKEN", errors, tree);
+  var NewNode = {parent:CurrentNode, value:"BLOCK", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
+  matchToken(tokens.shift(), "OPEN_BRACKET_TOKEN");
   parseStatementList();
-  matchToken(tokens.shift(), "CLOSE_BRACKET_TOKEN", errors, tree);
+  matchToken(tokens.shift(), "CLOSE_BRACKET_TOKEN");
+  CurrentNode = NewNode.parent;
 }
 
 function parseStatementList(){
+  var NewNode = {parent:CurrentNode, value:"STATEMENT LIST", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   if((tokens[0].type == "PRINT_TOKEN") || (tokens[0].type == "ID_TOKEN") || (tokens[0].type == "VAR_TYPE_TOKEN") || (tokens[0].type == "WHILE_TOKEN") || (tokens[0].type == "IF_TOKEN") || (tokens[0].type == "OPEN_BRACKET_TOKEN")){
     parseStatement();
     parseStatementList();
@@ -41,9 +49,13 @@ function parseStatementList(){
   else{
     //Epsilon Production
   }
+  CurrentNode = NewNode.parent;
 }
 
 function parseStatement(){
+  var NewNode = {parent:CurrentNode, value:"STATEMENT", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   if(tokens[0].type == "PRINT_TOKEN"){
     parsePrintStatement();
   }
@@ -62,39 +74,63 @@ function parseStatement(){
   else if(tokens[0].type == "OPEN_BRACKET_TOKEN"){
     parseBlock();
   }
+  CurrentNode = NewNode.parent;
 }
 
 function parsePrintStatement(){
+  var NewNode = {parent:CurrentNode, value:"PRINT STATEMENT", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   matchToken(tokens.shift(), "PRINT_TOKEN");
   matchToken(tokens.shift(), "OPEN_PAREN_TOKEN");
   parseExpr();
   matchToken(tokens.shift(), "CLOSE_PAREN_TOKEN");
+  CurrentNode = NewNode.parent;
 }
 
 function parseAssignmentStatement(){
+  var NewNode = {parent:CurrentNode, value:"ASSIGNMENT STATEMENT", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   parseId();
   matchToken(tokens.shift(), "ASSIGNMENT_TOKEN");
   parseExpr();
+  CurrentNode = NewNode.parent;
 }
 
 function parseVarDecl(){
+  var NewNode = {parent:CurrentNode, value:"VARIABLE DECLARATION", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   matchToken(tokens.shift(), "VAR_TYPE_TOKEN");
   parseId();
+  CurrentNode = NewNode.parent;
 }
 
 function parseWhileStatement(){
+  var NewNode = {parent:CurrentNode, value:"WHILE STATEMENT", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   matchToken(tokens.shift(), "WHILE_TOKEN");
   parseBooleanExpr();
   parseBlock();
+  CurrentNode = NewNode.parent;
 }
 
 function parseIfStatement(){
+  var NewNode = {parent:CurrentNode, value:"IF STATEMENT", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   matchToken(tokens.shift(), "IF_TOKEN");
   parseBooleanExpr();
   parseBlock();
+  CurrentNode = NewNode.parent;
 }
 
 function parseExpr(){
+  var NewNode = {parent:CurrentNode, value:"EXPRESSION", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   if(tokens[0].type == "INTEGER_TOKEN"){
     parseIntExpr();
   }
@@ -107,23 +143,35 @@ function parseExpr(){
   else if(tokens[0].type == "ID_TOKEN"){
     parseId();
   }
+  CurrentNode = NewNode.parent;
 }
 
 function parseIntExpr(){
+  var NewNode = {parent:CurrentNode, value:"INTEGER EXPRESSION", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   matchToken(tokens.shift(), "INTEGER_TOKEN");
   if(tokens[0].type == "ADDITION_TOKEN"){
     matchToken(tokens.shift(), "ADDITION_TOKEN");
     parseExpr();
   }
+  CurrentNode = NewNode.parent;
 }
 
 function parseStringExpr(){
+  var NewNode = {parent:CurrentNode, value:"STRING EXPRESSION", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   matchToken(tokens.shift(), "QUOTE_TOKEN");
   parseCharList();
   matchToken(tokens.shift(), "QUOTE_TOKEN");
+  CurrentNode = NewNode.parent;
 }
 
 function parseBooleanExpr(){
+  var NewNode = {parent:CurrentNode, value:"BOOLEAN EXPRESSION", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   if(tokens[0].type == "OPEN_PAREN_TOKEN"){
     matchToken(tokens.shift(), "OPEN_PAREN_TOKEN");
     parseExpr();
@@ -134,30 +182,47 @@ function parseBooleanExpr(){
   else {
     parseBoolVal();
   }
+  CurrentNode = NewNode.parent;
 }
 
 function parseId(){
+  var NewNode = {parent:CurrentNode, value:"ID", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   matchToken(tokens.shift(), "ID_TOKEN");
+  CurrentNode = NewNode.parent;
 }
 
 function parseCharList(){
+  var NewNode = {parent:CurrentNode, value:"CHAR LIST", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   if(tokens[0].type == "CHARACTER_TOKEN"){
     matchToken(tokens.shift(), "CHARACTER_TOKEN");
   }
   else {
     //Epsilon Production
   }
+  CurrentNode = NewNode.parent;
 }
 
 function parseBoolOp(){
+  var NewNode = {parent:CurrentNode, value:"BOOLEAN OPERATION", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   if(tokens[0].type == "INEQUALITY_COMPARATOR_TOKEN"){
     matchToken(tokens.shift(), "INEQUALITY_COMPARATOR_TOKEN");
   }
   else{
     matchToken(tokens.shift(), "EQUALITY_COMPARATOR_TOKEN");
   }
+  CurrentNode = NewNode.parent;
 }
 
 function parseBoolVal(){
+  var NewNode = {parent:CurrentNode, value:"BOOLEAN VALUE", children:[]};
+  CurrentNode.children.push(NewNode);
+  CurrentNode = NewNode;
   matchToken(tokens.shift(), "BOOL_VAL_TOKEN");
+  CurrentNode = NewNode.parent;
 }
