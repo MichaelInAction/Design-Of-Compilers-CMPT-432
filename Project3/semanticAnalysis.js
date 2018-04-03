9a93f2a1714a571faad4b02225ccb2aa41777771
MichaelInAction <michael.read.75@gmail.com>
2018-04-03 00:06:45 -0400

    4/3/2018 - Project3
    Updated semanticAnalysis.js to be recursive, to make spaghetti stack generation easier. Began working on the recursive calls necessary to get through semantic analysis. Fun times were had all around.

--- Project3/semanticAnalysis.js
+++ Project3/semanticAnalysis.js
@@ -3,35 +3,149 @@ var symbols;
 var warnings;
 var errors;
 var output;
+var count;
+var scopes;
+var currentScope;
+var nextScope;
+var CurrentNode;
 
-function checkSemantics(tokens, symbols, warnings, errors){
+function checkSemantics(tokens, FirstNode, symbols, warnings, errors){
   console.log("Begin Semantic Analysis...");
   output = "";
-  var scopes = [];
-  var currentScope = 0;
-  var nextScope = 1;
+  scopes = [];
+  currentScope = 0;
+  nextScope = 1;
+  count = 0;
   this.tokens = tokens;
+  CurrentNode = FirstNode;
   this.symbols = symbols;
   this.warnings = warnings;
   this.errors = errors;
-  for(var i = 0; i < tokens.length; i++){
-    output = output + tokens[i].value + currentScope + "\n";
-    if(tokens[i].value == "{"){
-      var temp = {scope:nextScope, outerScope:currentScope};
-      scopes.push(temp);
-      currentScope = nextScope;
-      nextScope = nextScope + 1;
-      continue;
-    }
-    if(tokens[i].value == "}"){
-      for(var j = 0; j < scopes.length; j++){
-        if(scopes[j].scope == currentScope){
-          currentScope = scopes[j].outerScope;
-          break;
-        }
-      }
-      continue;
-    }
-  }
+  block();
   return output;
+}
+
+function block(){
+  if(errors.length == 0){
+    console.log("SA: Block");
+    //Next token is a {
+    count++;
+    var temp = {scope:nextScope, outerScope:currentScope};
+    scopes.push(temp);
+    currentScope = nextScope;
+    nextScope = nextScope + 1;
+    var NewNode = {parent:CurrentNode, value:"BLOCK", children:[]};
+    CurrentNode.children.push(NewNode);
+    CurrentNode = NewNode;
+    statementList();
+    CurrentNode = NewNode.parent;
+    currentScope = temp.outerScope;
+    //Next token is a }
+    count++;
+  }
+}
+
+function statementList(){
+  if((tokens[count].type == "PRINT_TOKEN") || (tokens[count].type == "ID_TOKEN") || (tokens[count].type == "VAR_TYPE_TOKEN") || (tokens[count].type == "WHILE_TOKEN") || (tokens[count].type == "IF_TOKEN") || (tokens[count].type == "OPEN_BRACKET_TOKEN")){
+    console.log("SA: Statement List");
+    count++;
+    statement();
+    statementList();
+  }
+  else{
+    //Epsilon Production
+  }
+}
+
+function statement(){
+  if(tokens[count].type == "PRINT_TOKEN"){
+    printStatement();
+  }
+  else if(tokens[count].type == "ID_TOKEN"){
+    sssignmentStatement();
+  }
+  else if(tokens[count].type == "VAR_TYPE_TOKEN"){
+    varDecl();
+  }
+  else if(tokens[count].type == "WHILE_TOKEN"){
+    whileStatement();
+  }
+  else if(tokens[count].type == "IF_TOKEN"){
+    ifStatement();
+  }
+  else if(tokens[count].type == "OPEN_BRACKET_TOKEN"){
+    block();
+  }
+}
+
+function printStatement(){
+
+}
+
+function assignmentStatement(){
+
+}
+
+function varDecl(){
+
+}
+
+function whileStatement(){
+
+}
+
+function ifStatement(){
+
+}
+
+function expr(){
+
+}
+
+function intExpr(){
+
+}
+
+function stringExpr(){
+
+}
+
+function booleanExpr(){
+
+}
+
+function id(){
+
+}
+
+function charList(){
+
+}
+
+function type(){
+
+}
+
+function char(){
+
+}
+
+function space(){
+
+}
+
+function digit(){
+
+}
+
+function boolop(){
+
+}
+
+function boolval(){
+
+}
+
+function intop(){
+
 }
