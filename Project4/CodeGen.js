03ffb46f3c98c993289b55f29f36068d00f0b376
MichaelInAction <michael.read.75@gmail.com>
2018-05-08 23:42:39 -0400

    5/8/2018 - Project4
    Printing ids now works, and assignment is on its way. This is oh so very exciting

--- Project4/CodeGen.js
+++ Project4/CodeGen.js
@@ -7,6 +7,7 @@ var currentByte;
 var heapByte;
 var currentScope;
 var maximumScope;
+var scopeTable;
 var staticStorageByte;
 var staticDataTable;
 var jumpsTable;
@@ -15,11 +16,14 @@ function generateCode(tokens, warnings, errors){
   this.tokens = tokens;
   this.warnings = warnings;
   this.errors = errors;
+  currentScope = null;
+  maximumScope = 0;
   count = 0;
   currentByte = 0;
   heapByte = 244;
   staticStorageByte = 0;
   staticDataTable = [];
+  scopeTable = [];
   jumpsTable = [];
   output = [ "00", "00", "00", "00", "00", "00", "00", "00",
              "00", "00", "00", "00", "00", "00", "00", "00",
@@ -114,9 +118,11 @@ function codeblock(){
     console.log("CG: Block");
     //Next token is a {
     count++;
-    var temp = currentScope;
     maximumScope = maximumScope + 1;
-    currentScope = maximumScope;
+    var newScope = {num:maximumScope, parent:currentScope};
+    var temp = currentScope;
+    currentScope = newScope;
+    scopeTable.push(newScope);
     codestatementList();
     currentScope = temp;
     //Next token is a }
@@ -250,28 +256,122 @@ function codeprintStatement(){
     currentByte = currentByte + 1;
   }
   else if(tokens[count].type == "ID_TOKEN"){
-
+    var id = tokens[count].value;
+    var tempScope = currentScope;
+    var found = false;
+    var index = 0;
+    while(tempScope != null){
+      for(var i = 0; i < staticDataTable.length; i++){
+        if(staticDataTable[i].scope == currentScope.num && staticDataTable[i].name == id){
+          found = true;
+          index = i;
+          break;
+        }
+      }
+      if(found){
+        break;
+      }
+    }
+    if(staticDataTable[index].type == "int"){
+      output[currentByte] = "A2";
+      currentByte = currentByte + 1;
+      output[currentByte] = "01";
+      currentByte = currentByte + 1;
+      output[currentByte] = "AC";
+      currentByte = currentByte + 1;
+      output[currentByte] = staticDataTable[index].tempLoc;
+      currentByte = currentByte + 1;
+      output[currentByte] = "00";
+      currentByte = currentByte + 1;
+      output[currentByte] = "FF";
+      currentByte = currentByte + 1;
+    }
+    else{
+      output[currentByte] = "A2";
+      currentByte = currentByte + 1;
+      output[currentByte] = "02";
+      currentByte = currentByte + 1;
+      output[currentByte] = "AC";
+      currentByte = currentByte + 1;
+      output[currentByte] = staticDataTable[index].tempLoc;
+      currentByte = currentByte + 1;
+      output[currentByte] = "00";
+      currentByte = currentByte + 1;
+      output[currentByte] = "FF";
+      currentByte = currentByte + 1;
+    }
+    count++;
   }
   count++;
 }
 
 function codeassignmentStatement(){
-  console.log(tokens[count].value);
-  var staticTableLocation = 0;
-  for(var i = 0; i < staticDataTable.length; i++){
-    if(tokens[count].value === staticDataTable[i]){
-
+  var id = tokens[count].value;
+  var tempScope = currentScope;
+  var found = false;
+  var index = 0;
+  while(tempScope != null){
+    for(var i = 0; i < staticDataTable.length; i++){
+      if(staticDataTable[i].scope == currentScope.num && staticDataTable[i].name == id){
+        found = true;
+        index = i;
+        break;
+      }
+    }
+    if(found){
+      break;
     }
   }
+  if(staticDataTable[index].type == "int"){
+    output[currentByte] = "A9";
+    currentByte = currentByte + 1;
+    output[currentByte] = "";
+    currentByte = currentByte + 1;
+    output[currentByte] = "8D";
+    currentByte = currentByte + 1;
+    output[currentByte] = staticDataTable[index].tempLoc;
+    currentByte = currentByte + 1;
+    output[currentByte] = "00";
+    currentByte = currentByte + 1;
+  }
+  else if(staticDataTable[index].type == "boolean"){
+    if(tokens[count+2].value == "true"){
+      output[currentByte] = "A9";
+      currentByte = currentByte + 1;
+      output[currentByte] = "F4";
+      currentByte = currentByte + 1;
+      output[currentByte] = "8D";
+      currentByte = currentByte + 1;
+      output[currentByte] = staticDataTable[index].tempLoc;
+      currentByte = currentByte + 1;
+      output[currentByte] = "00";
+      currentByte = currentByte + 1;
+    }
+    else{
+      output[currentByte] = "A9";
+      currentByte = currentByte + 1;
+      output[currentByte] = "F9";
+      currentByte = currentByte + 1;
+      output[currentByte] = "8D";
+      currentByte = currentByte + 1;
+      output[currentByte] = staticDataTable[index].tempLoc;
+      currentByte = currentByte + 1;
+      output[currentByte] = "00";
+      currentByte = currentByte + 1;
+    }
+  }
+  else{
 
+  }
   count++;
   count++;
-  codeexpr();
+  count++;
+  //codeexpr();
 }
 
 function codevarDecl(){
 
-  var declaredVariable = {name:tokens[count+1].value, type:tokens[count].value, scope:currentScope, tempLoc: "T" + (staticDataTable.length + 1), memoryLoc:""};
+  var declaredVariable = {name:tokens[count+1].value, type:tokens[count].value, scope:currentScope.num, tempLoc: "T" + (staticDataTable.length + 1), memoryLoc:""};
   output[currentByte] = "A9";
   currentByte = currentByte + 1;
   if(declaredVariable.type == "boolean"){
@@ -299,22 +399,14 @@ function codevarDecl(){
 
 function codewhileStatement(){
   count++;
-  var NewNode = {parent:CurrentNode, value:"While", children:[]};
-  CurrentNode.children.push(NewNode);
-  CurrentNode = NewNode;
   codebooleanExpr();
   codeblock();
-  CurrentNode = NewNode.parent;
 }
 
 function codeifStatement(){
   count++;
-  var NewNode = {parent:CurrentNode, value:"If", children:[]};
-  CurrentNode.children.push(NewNode);
-  CurrentNode = NewNode;
   codebooleanExpr();
   codeblock();
-  CurrentNode = NewNode.parent;
 }
 
 function codeexpr(){
@@ -322,171 +414,42 @@ function codeexpr(){
     codeintExpr();
   }
   else if(tokens[count].type == "QUOTE_TOKEN"){
-    codestringExpr();
+
   }
   else if(tokens[count].type == "OPEN_PAREN_TOKEN" || tokens[count].type == "BOOL_VAL_TOKEN"){
     codebooleanExpr();
   }
   else if(tokens[count].type == "ID_TOKEN"){
-    var varNode = {parent:CurrentNode, value:tokens[count].value, children:[]};
-    CurrentNode.children.push(varNode);
-    var alreadyDeclared = false;
-    var tempScope = currentScope;
-    while(tempScope != null){
-      console.log("checking scope");
-      for(var i = 0; i < tempScope.variables.length; i++){
-        console.log(tempScope.variables[i].id);
-        if(tempScope.variables[i].id == varNode.value){
-
-          alreadyDeclared = true;
-          break;
-        }
-      }
-      if(alreadyDeclared){
-        break;
-      }
-      tempScope = tempScope.outerScope;
-    }
-    if(!alreadyDeclared){
-      var error = {type:"UNDECLARED_VARIABLE_ERROR", line:tokens[count].line};
-      errors.push(error);
-      output = output + "SA: UNDECLARED_VARIABLE_ERROR! Variable " +
-      varNode.value + " in scope " + currentScope.scopeNum + " on line "
-      + tokens[count].line + "\n";
-    }
     count++;
-  }
-}
-
-function codetypeCheckExpr(type){
-  if(tokens[count].type == "INTEGER_TOKEN" && type == "int"){
-    codeintExpr();
-  }
-  else if(tokens[count].type == "QUOTE_TOKEN" && type == "string"){
-    codestringExpr();
-  }
-  else if((tokens[count].type == "OPEN_PAREN_TOKEN" || tokens[count].type == "BOOL_VAL_TOKEN") && type == "boolean"){
-    codebooleanExpr();
-  }
-  else if(tokens[count].type == "ID_TOKEN"){
-    var varNode = {parent:CurrentNode, value:tokens[count].value, children:[]};
-    CurrentNode.children.push(varNode);
-    var alreadyDeclared = false;
-    var tempScope = currentScope;
-    var tempType = "";
-    while(tempScope != null){
-      console.log("checking scope");
-      for(var i = 0; i < tempScope.variables.length; i++){
-        console.log(tempScope.variables[i].id);
-        if(tempScope.variables[i].id == varNode.value){
-          tempType = tempScope.variables[i].type;
-          alreadyDeclared = true;
-          break;
-        }
-      }
-      if(alreadyDeclared){
-        break;
-      }
-      tempScope = tempScope.outerScope;
-    }
-    if(!alreadyDeclared){
-      var error = {type:"UNDECLARED_VARIABLE_ERROR", line:tokens[count].line};
-      errors.push(error);
-      output = output + "SA: UNDECLARED_VARIABLE_ERROR! Variable " +
-      varNode.value + " in scope " + currentScope.scopeNum + " on line "
-      + tokens[count].line + "\n";
-    }
-    if(type != tempType){
-      var error = {type:"TYPE_MISMATCH_ERROR", line:tokens[count].line};
-      errors.push(error);
-      output = output + "SA: TYPE_MISMATCH_ERROR! Variable " +
-      varNode.value + " in scope " + currentScope.scopeNum + " on line "
-      + tokens[count].line + ". Found type " + tempType + ", expected type "
-      + type + "\n";
-    }
-    count++;
-  }
-  else{
-    if(tokens[count].type == "INTEGER_TOKEN"){
-      var error = {type:"TYPE_MISMATCH_ERROR", line:tokens[count].line};
-      errors.push(error);
-      output = output + "SA: TYPE_MISMATCH_ERROR! On line "
-      + tokens[count].line + ". Found type int, expected type "
-      + type + "\n";
-    }
-    else if(tokens[count].type == "QUOTE_TOKEN"){
-      var error = {type:"TYPE_MISMATCH_ERROR", line:tokens[count].line};
-      errors.push(error);
-      output = output + "SA: TYPE_MISMATCH_ERROR! On line "
-      + tokens[count].line + ". Found type string, expected type "
-      + type + "\n";
-    }
-    else{
-      var error = {type:"TYPE_MISMATCH_ERROR", line:tokens[count].line};
-      errors.push(error);
-      output = output + "SA: TYPE_MISMATCH_ERROR! On line "
-      + tokens[count].line + ". Found type boolean, expected type "
-      + type + "\n";
-    }
   }
 }
 
 function codeintExpr(){
   if(tokens[count+1].type == "ADDITION_TOKEN"){
-    var NewNode = {parent:CurrentNode, value:"Addition", children:[]};
-    CurrentNode.children.push(NewNode);
-    CurrentNode = NewNode;
-    var numNode = {parent:CurrentNode, value:tokens[count].value, children:[]};
-    CurrentNode.children.push(numNode);
     count++;
     count++;
-    codetypeCheckExpr("int");
-    CurrentNode = NewNode.parent;
+    codeexpr();
   }
   else{
-    var numNode = {parent:CurrentNode, value:tokens[count].value, children:[]};
-    CurrentNode.children.push(numNode);
     count++;
   }
-}
-
-function codestringExpr(){
-  count++;
-  codecharList();
-  count++;
 }
 
 function codebooleanExpr(){
   if(tokens[count].type == "OPEN_PAREN_TOKEN"){
     count++;
     if(tokens[count+1].type == "INEQUALITY_COMPARATOR_TOKEN"){
-      var NewNode = {parent:CurrentNode, value:"Not Equals", children:[]};
-      CurrentNode.children.push(NewNode);
-      CurrentNode = NewNode;
+
     }
     else{
-      var NewNode = {parent:CurrentNode, value:"Is Equal", children:[]};
-      CurrentNode.children.push(NewNode);
-      CurrentNode = NewNode;
+
     }
     codeexpr();
     count++;
     codeexpr();
     count++;
-    CurrentNode = NewNode.parent;
   }
   else {
-    var NewNode = {parent:CurrentNode, value:tokens[count].value, children:[]};
-    CurrentNode.children.push(NewNode);
     count++
-  }
-}
-
-function codecharList(){
-  //This
-  var tempString = "";
-  while(tokens[count].type != "QUOTE_TOKEN"){
-    tempString = tempString + tokens[count].value;
-    count++;
   }
 }
